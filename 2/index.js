bfd0188dbc1a9f878943f0cc646a52f365dbec1b
aleksei.petrov <iamalexey.dev@gmail.com>
2019-12-13 08:02:56 +0300

    WIP 4

--- 2/index.js
+++ 2/index.js
@@ -1,9 +1,14 @@
+let navigation = {
+    current: 1, 
+    maxPage: 1
+};
+
 function getUsersInit() {
     fetch('https://reqres.in/api/users?page=1')
         .then(response => response.json())
         .then(res => {
+            navigation.maxPage = res.total_pages;
             res.data.forEach(element => {
-                console.log(element);
                 $('.listUsers').append(
                     `<div class="media">
                     <div class="media-body">
@@ -17,3 +22,53 @@ function getUsersInit() {
 };
 
 getUsersInit();
+
+$('.next').click(function() {
+    if (navigation.current !== navigation.maxPage) {
+        navigation.current += 1;
+        $('.listUsers').empty();
+        fetch(`https://reqres.in/api/users?page=${navigation.current}`)
+            .then(response => response.json())
+            .then(res => {
+                navigation.maxPage = res.total_pages;
+                res.data.forEach(element => {
+                    $('.listUsers').append(
+                        `<div class="media">
+                        <div class="media-body">
+                         ${element.first_name} ${element.last_name}
+                        </div>
+                        <img src="${element.avatar}" class="ml-3" alt="...">
+                      </div>`
+                    );
+                });
+
+                if (navigation.current === navigation.maxPage) {
+                    $('.prev-block').removeClass('disable');
+                    $('.next-block').addClass('disable');
+                }
+            });
+    }
+});
+
+
+$('.prev').click(function() {
+    if (navigation.current !== 1) {
+        navigation.current -= 1;
+        $('.listUsers').empty();
+        fetch(`https://reqres.in/api/users?page=${navigation.current}`)
+            .then(response => response.json())
+            .then(res => {
+                navigation.maxPage = res.total_pages;
+                res.data.forEach(element => {
+                    $('.listUsers').append(
+                        `<div class="media">
+                        <div class="media-body">
+                        ${element.first_name} ${element.last_name}
+                        </div>
+                        <img src="${element.avatar}" class="ml-3" alt="...">
+                    </div>`
+                    );
+                });
+            });
+        }
+});
