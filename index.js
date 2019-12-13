4784a802f5993ba2955361b68b42358577335ebd
aleksei.petrov <iamalexey.dev@gmail.com>
2019-12-13 07:08:23 +0300

    second part fix

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ $('.form').submit(function(e) {
     const number1Str = $('#number1').val();
     const number2Str = $("#number2").val();
 
-    if (+number1Str > +number2Str) {
+    if ((+number1Str < 2 || +number1Str > 100) || (+number2Str < 2 || +number2Str > 100)) {
         $('.error').text('Invalid input, please try again');
+    } else {
+        $('.error').text('');
     }
-
 });
